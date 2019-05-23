b456330e822d258dcb6848faa87428ebe2ffed85
gayyou <zxc365912613@qq.com>
2019-05-23 19:46:53 +0800

    修改

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,17 @@
+const webpack = require("webpack");
+
 module.exports = {
-  lintOnSave: false
+  configureWebpack: {
+        plugins: [
+            new webpack.ProvidePlugin({
+                $: 'jquery',
+                jQuery: 'jquery',
+                'window.jQuery': 'jquery',
+                Popper: ['popper.js', 'default']
+              })
+        ]
+      },
+  publicPath: './',
+  lintOnSave: false 
+
 }
